5ebca5ad91f80e8f01f66b301ef71e2dbc70364a
irfan-rumon <irfannisho8571@gmail.com>
2022-11-22 13:07:27 +0600

    Add team members section

---
+++ wp-content/themes/picchi/team.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * The template for displaying the welcome page
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package picchi
+ */
+
+?>
+
+<div class="about-section">
+	  <h1 class="about-heading">Our Team Members</h1>	
+
+      
+
+    <div class="team-section">
+            <div class="team-member">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1591084728795-1149f32d9866?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">View Details</a>
+                </div>
+            </div>
+
+            <div class="team-member">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">View Details</a>
+                </div>
+            </div>
+
+
+            <div class="team-member">
+                <img class="card-img-top" src="https://images.unsplash.com/photo-1566753323558-f4e0952af115?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1021&q=80" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">View Details</a>
+                </div>
+</div>
+
+            
+            
+
+    </div>
+        
+      
+            
+
+</div>
+
+
+
+    
+
+
+      </div>
+
+
+</div>
+
+
+
+
+</body>
+</html>
---
+++ wp-content/themes/twentytwentyone/team.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the welcome page
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package picchi
+ */
+
+?>
+
+<div class="about-section">
+	  <h1 class="about-heading">Our Team Members</h1>	
+
+      
+            
+
+
+      </div>
+
+
+</div>
+
+
+
+
+</body>
+</html>
